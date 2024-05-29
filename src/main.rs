1f68b1a1f68305b35e87f66d95c5f76559e8bd90
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-29 14:03:00 +0530

    Refactoring and Lifetimes with Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::env;
+use std::process;
 use std::fs;
 
+struct Config<'a> {
+    query: &'a String,
+    file_path: &'a String,
+}
+
+impl<'a> Config<'a> {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+        let query: &String = &args[1];
+        let file_path: &String = &args[2];
+
+        Ok(Config { query, file_path })
+    }
+}
+
 fn main() {
-
     let args: Vec<String> = env::args().collect();
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In fileS {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file!");
-
-    println!("Text Found: \n{contents}");
+    let contents = fs::read_to_string(config.file_path).expect("Error!");
+    println!("{}", contents);
 }
